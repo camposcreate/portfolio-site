7401c3d501cf540eb3250a76cc1d3cac8c559f3e
camposcreate <uricampos123@gmail.com>
2023-07-19 21:03:45 -0500

    Contact section implemented, minor nav bar updates, and some positioning changes

--- src/script.js
+++ src/script.js
@@ -1,18 +1,34 @@
-window.addEventListener("scroll", function () {
-  const navLinks = document.querySelectorAll(".nav-link");
+window.addEventListener('DOMContentLoaded', function() {
+    // Get the navigation links
+    var navLinks = document.querySelectorAll('.nav-link');
 
-  navLinks.forEach(function (link) {
-    const sectionId = link.getAttribute("href");
-    const section = document.querySelector(sectionId);
+    // Handle scroll event
+    window.addEventListener('scroll', function() {
+        // Get the current scroll position
+        var scrollPosition = window.scrollY;
 
-    const rect = section.getBoundingClientRect();
-    if (
-      rect.top >= 0 &&
-      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight)
-    ) {
-      link.classList.add("active");
-    } else {
-      link.classList.remove("active");
-    }
-  });
+        // Loop through the sections
+        document.querySelectorAll('section').forEach(function(section) {
+            var sectionId = section.getAttribute('id');
+            var sectionOffset = section.offsetTop;
+            var sectionHeight = section.offsetHeight;
+
+            // Add a slight offset to the section activation
+            var activationOffset = 70;
+
+            // Check if the scroll position is within the section with the offset
+            if (
+                scrollPosition >= sectionOffset - activationOffset &&
+                scrollPosition < sectionOffset + sectionHeight - activationOffset
+            ) {
+                // Activate the corresponding navigation item
+                navLinks.forEach(function(link) {
+                    link.classList.remove('active');
+                    if (link.getAttribute('href') === '#' + sectionId) {
+                        link.classList.add('active');
+                    }
+                });
+            }
+        });
+    });
 });
