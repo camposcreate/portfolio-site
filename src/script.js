55daa7f893fc78daf218d766ad7732f10a29e74d
camposcreate <uricampos123@gmail.com>
2023-08-01 21:05:14 -0500

    Nav bar scroll progress bar, minor positioning changes

--- src/script.js
+++ src/script.js
@@ -1,3 +1,15 @@
+// function updates scroll progress bar
+function updateProgressBar() {
+    const documentHeight = document.documentElement.scrollHeight;
+    const viewportHeight = window.innerHeight;
+    const scrollProgress = window.scrollY / (documentHeight - viewportHeight);
+    const progressBarWidth = scrollProgress * 100;
+    const progressBar = document.querySelector('.filled');
+    progressBar.style.width = progressBarWidth + '%';
+}
+window.addEventListener('scroll', updateProgressBar);
+
+
 window.addEventListener('DOMContentLoaded', function() {
     // Get the navigation links
     var navLinks = document.querySelectorAll('.nav-link');
@@ -14,7 +26,7 @@ window.addEventListener('DOMContentLoaded', function() {
             var sectionHeight = section.offsetHeight;
 
             // Add a slight offset to the section activation
-            var activationOffset = 70;
+            // var activationOffset = 100;
 
             // Check if the scroll position is within the section with the offset
             if (
@@ -31,4 +43,18 @@ window.addEventListener('DOMContentLoaded', function() {
             }
         });
     });
+
+      // Add event listener to the 'Home' link
+      // Clicking 'Home' correctly sets user to top of page
+      var homeLink = document.querySelector('.nav-link.home-link');
+      homeLink.addEventListener('click', function(event) {
+        event.preventDefault(); // Prevent the default link behavior
+        var homeSection = document.querySelector('#home');
+        var scrollToPosition = homeSection.offsetTop;
+        // Smoothly scroll to the top of the 'Home' section
+        (window.scrollTo({
+          top: scrollToPosition
+        }));
+      });
+
 });
