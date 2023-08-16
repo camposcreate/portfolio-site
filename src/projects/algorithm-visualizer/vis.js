2f41da71743209b70aa1db032cbddabaa7c62e27
camposcreate <uricampos123@gmail.com>
2023-08-15 23:28:20 -0500

    Visualizer grid improvements.. added sprites with user control

--- src/projects/algorithm-visualizer/vis.js
+++ src/projects/algorithm-visualizer/vis.js
@@ -1,3 +1,4 @@
+/*
 // toggle item color and reset board
 document.addEventListener('DOMContentLoaded', () => {
     const gridItems = document.querySelectorAll('.grid a');
@@ -17,4 +18,35 @@ document.addEventListener('DOMContentLoaded', () => {
             item.parentElement.classList.remove('clicked');
         });
     });
+});*/
+
+const cell = document.querySelectorAll(".grid-item"),
+    image = document.querySelector(".image");
+
+console.log(cell, image);
+
+// loop each cell element
+cell.forEach((gridItem) => {
+    // When elements are dragged over cells
+    gridItem.addEventListener("dragover", (e) => {
+        e.preventDefault();
+        gridItem.classList.add("hovered");
+    });
+
+    // When elements are dragged out of cells
+    gridItem.addEventListener("dragleave", () => {
+        gridItem.classList.remove("hovered");
+    });
+
+    // When elements are dropped in a cell
+    gridItem.addEventListener("drop", () => {
+        gridItem.appendChild(image);
+        gridItem.classList.remove("hovered");
+    });
 });
+
+
+
+
+
+
