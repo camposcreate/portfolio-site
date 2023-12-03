f075dff030368cae13e7030817333057fa4d5724
camposcreate <uricampos123@gmail.com>
2023-12-02 19:23:01 -0600

    Mobile functionality complete

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -9,53 +9,14 @@ function updateProgressBar() {
 }
 window.addEventListener('scroll', updateProgressBar);
 
+// Open/close side menu for mobile view
+function openmenu() {
+    sidemenu.classList.add("menu-open");
+}
 
-/*window.addEventListener('DOMContentLoaded', function() {
-    // Get the navigation links
-    var navLinks = document.querySelectorAll('.nav-link');
+function closemenu() {
+    sidemenu.classList.remove("menu-open");
+}
 
-    // Handle scroll event
-    window.addEventListener('scroll', function() {
-        // Get the current scroll position
-        var scrollPosition = window.scrollY;
 
-        // Loop through the sections
-        document.querySelectorAll('section').forEach(function(section) {
-            var sectionId = section.getAttribute('id');
-            var sectionOffset = section.offsetTop;
-            var sectionHeight = section.offsetHeight;
 
-            // Add a slight offset to the section activation
-            var activationOffset = 100;
-
-            // Check if the scroll position is within the section with the offset
-            if (
-                scrollPosition >= sectionOffset - activationOffset &&
-                scrollPosition < sectionOffset + sectionHeight - activationOffset
-            ) {
-                // Activate the corresponding navigation item
-                navLinks.forEach(function(link) {
-                    link.classList.remove('active');
-                    if (link.getAttribute('href') === '#' + sectionId) {
-                        link.classList.add('active');
-                    }
-                });
-            }
-        });
-    });
-
-      // Add event listener to the 'Home' link
-      // Clicking 'Home' correctly sets user to top of page
-      var homeLink = document.querySelector('.nav-link.home-link');
-      homeLink.addEventListener('click', function(event) {
-        event.preventDefault(); // Prevent the default link behavior
-        var homeSection = document.querySelector('#home');
-        var scrollToPosition = homeSection.offsetTop;
-        // Smoothly scroll to the top of the 'Home' section
-        (window.scrollTo({
-          top: scrollToPosition
-        }));
-      });
-
-});
-*/
