01a706fa38521df586737b274917b001dfe95ade
camposcreate <uricampos123@gmail.com>
2023-12-03 21:49:51 -0600

    Contact form enabled

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -18,5 +18,22 @@ function closemenu() {
     sidemenu.classList.remove("menu-open");
 }
 
+document.addEventListener('DOMContentLoaded', function () {
+  const scriptURL = 'https://script.google.com/macros/s/AKfycbwpAgKQRx-iSv9C7LEIhiey-h6HGEDa8Sk8ZaKz-BnWIJohVk4t642AywpOU1DTYpqu8A/exec';
+  const form = document.forms['submit-to-google-sheet'];
+  const msg = document.getElementById("message-pop-up")
 
-
+  form.addEventListener('submit', e => {
+    e.preventDefault();
+    fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+      .then(response => {
+        msg.innerHTML = "Message sent successfully!"
+        setTimeout(function(){
+            console.log('Success!', response)
+            msg.innerHTML = ""
+        },4000) // 4 secs
+        form.reset()
+      })
+      .catch(error => console.error('Error!', error.message));
+  });
+});
