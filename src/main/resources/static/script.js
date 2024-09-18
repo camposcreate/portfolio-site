bddb71477ff7c9970c8ae603a3d9b7b9b6e2d4a7
camposcreate <uricampos123@gmail.com>
2024-09-18 18:31:39 -0500

    visual changes + contact bugs resolved

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -35,14 +35,6 @@ const observer = new IntersectionObserver((entries) => {
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
 
-// open/close side menu for mobile view
-function openmenu() {
-    sidemenu.classList.add("menu-open");
-}
-function closemenu() {
-    sidemenu.classList.remove("menu-open");
-}
-
 // contact form
 document.addEventListener('DOMContentLoaded', function () {
   const scriptURL = 'https://script.google.com/macros/s/AKfycbwpAgKQRx-iSv9C7LEIhiey-h6HGEDa8Sk8ZaKz-BnWIJohVk4t642AywpOU1DTYpqu8A/exec';
@@ -57,7 +49,7 @@ document.addEventListener('DOMContentLoaded', function () {
         setTimeout(function(){
             console.log('Success!', response)
             msg.innerHTML = ""
-        },4000) // 4 secs
+        },2000) // 4 secs
         form.reset()
       })
       .catch(error => console.error('Error!', error.message));
